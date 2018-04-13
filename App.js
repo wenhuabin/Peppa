59613b36e372d8824e3813f808445da81234fa2d
huabinwen <huabinwen@gmail.com>
2018-04-13 18:38:41 +0800

    learning...

--- App.js
+++ App.js
@@ -1,23 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {text: ''};
+    }
+
+    _onPressButton() {
+        Alert.alert('You tapped the button!')
+    }
+
+    render() {
+      return (
+          <View style={styles.container}>
+            <View style={styles.buttonContainer}>
+              <Button
+                onPress={this._onPressButton}
+                title="Press Me"
+              />
+            </View>
+            <View style={styles.buttonContainer}>
+              <Button
+                onPress={this._onPressButton}
+                title="Press Me"
+                color="#841584"
+              />
+            </View>
+            <View style={styles.alternativeLayoutButtonContainer}>
+              <Button
+                onPress={this._onPressButton}
+                title="This looks great!"
+              />
+              <Button
+                onPress={this._onPressButton}
+                title="OK!"
+                color="#841584"
+              />
+            </View>
+          </View>
+      );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+     flex: 1,
+     justifyContent: 'center',
+    },
+    buttonContainer: {
+      margin: 20
+    },
+    alternativeLayoutButtonContainer: {
+      margin: 20,
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    }
+})

3718fd2fc29d5daf1961df58ba4bf8015631a1aa
huabinwen <huabinwen@gmail.com>
2018-04-14 14:41:17 +0800

    add scroll view

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+import { Alert, AppRegistry, Button, StyleSheet, ScrollView, Image, View , Text} from 'react-native';
 
 export default class App extends React.Component {
     constructor(props) {
@@ -13,32 +13,13 @@ export default class App extends React.Component {
 
     render() {
       return (
-          <View style={styles.container}>
-            <View style={styles.buttonContainer}>
-              <Button
-                onPress={this._onPressButton}
-                title="Press Me"
-              />
-            </View>
-            <View style={styles.buttonContainer}>
-              <Button
-                onPress={this._onPressButton}
-                title="Press Me"
-                color="#841584"
-              />
-            </View>
-            <View style={styles.alternativeLayoutButtonContainer}>
-              <Button
-                onPress={this._onPressButton}
-                title="This looks great!"
-              />
-              <Button
-                onPress={this._onPressButton}
-                title="OK!"
-                color="#841584"
-              />
-            </View>
-          </View>
+          <ScrollView>
+            <Text style={{fontSize:96}}>Scroll me plz</Text>
+            <Text style={{fontSize:96}}>If you like</Text>
+            <Text style={{fontSize:96}}>Scrolling down</Text>
+            <Text style={{fontSize:96}}>What's the best</Text>
+            <Text style={{fontSize:80}}>React Native</Text>
+          </ScrollView>
       );
     }
 }
